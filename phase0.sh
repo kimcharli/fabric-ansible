37767a5290970b3c3fb2124ca23172f4f359aed9
Charlie (Chang-Hyun) Kim <ckim@juniper.net>
2019-05-10 08:04:48 -0400

    cleanup

--- phase0.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo mkdir -p /var/log/ansible
-sudo chown -R ${USER}: /var/log/ansible
-source ~/ENV/ansible-jnpr/bin/activate
-ls inventory/work > /dev/null || cp -r inventory/{quincy-example,work}
-
-
-
